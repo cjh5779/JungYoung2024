d118c3b29c125e7c7bf5b6b8277e63b483ad1300
Jung Hwan <choijh5779@gmail.com>
2024-11-29 23:07:11 +0900

    clone

--- Assets/Jung/Scripts/skill/young_Test.cs
+++ Assets/Jung/Scripts/skill/young_Test.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jung_Test : MonoBehaviour
+public class young_Test : MonoBehaviour
 {
     public ImgsFillDynamic ImgsFD;
     public GameObject ll;
-
     void Start()
     {
         this.ImgsFD.SetValue(0, true, 0.5f);
@@ -14,7 +13,7 @@ public class jung_Test : MonoBehaviour
     private void Update()
     {
 
-        this.ImgsFD.SetValue(ll.GetComponent<PlayerController>().jung_ballt / 4.0f, true, 0.5f);
+        this.ImgsFD.SetValue(ll.GetComponent<PlayerController>().young_ballt / 12.0f, true, 0.5f);
 
     }
 }
